32bcfc8186a564fcd00f353225449217a060db9d
Kapo <paykapo4@gmail.com>
2024-03-07 17:09:56 +0100

    push milestones

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,16 @@
+const container = document.querySelector("#container");
+let newLi;
+
 for(let i = 1; i <= 100; i++){
     if(i % 3 === 0 && i % 5 === 0){
-        console.log("FizzBuzz");
-    }else if (i % 3 === 0 && i % 5 !== 0){
-        console.log("Fizz");
-    }else if (i % 5 === 0){
-        console.log("Buzz");
-    }else {
-        console.log(i);
+        newLi = `<li class="box fizzbuzz">FizzBuzz</li>`;
+    } else if (i % 3 === 0){
+        newLi = `<li class="box fizz">Fizz</li>`;
+    } else if (i % 5 === 0){
+        newLi = `<li class="box buzz">Buzz</li>`;
+    } else {
+        newLi = `<li class="box">${i}</li>`;
     }
+
+    container.innerHTML += newLi;
 }
